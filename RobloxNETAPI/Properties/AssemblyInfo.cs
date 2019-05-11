7f5782c7e7120f586d0111cd9f513d63cda84334
oshawott9044 <oshawott9088@inbox.lv>
2019-05-11 15:32:48 +0200

    Update AssemblyInfo.cs

--- RobloxNETAPI/Properties/AssemblyInfo.cs
+++ RobloxNETAPI/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RobloxNETAPI")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("RobloxNET")]
+[assembly: AssemblyDescription("An unofficial asynchronus C# API for interacting with http://api.roblox.com/")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RobloxNETAPI")]
+[assembly: AssemblyProduct("RobloxNET")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
